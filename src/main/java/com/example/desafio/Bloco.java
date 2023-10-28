49284dad70bd674eb32629ebf9ea92cbf647f91a
Raphael Vacari <raphaelvacari7@gmail.com>
2023-10-28 20:20:19 -0300

    Criação da classe Bloco

---
+++ src/main/java/com/example/desafio/Bloco.java
@@ -0,0 +1,48 @@
+package com.example.desafio;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+    @Id
+    private Integer id;
+    private Instant inicio;
+    private Instant fim;
+
+    public Bloco() {
+    }
+
+    public Bloco(Integer id, Instant inicio, Instant fim) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+}
